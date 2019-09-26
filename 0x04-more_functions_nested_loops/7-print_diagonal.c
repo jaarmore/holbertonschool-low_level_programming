72768a00c656e9aa4eb592ca8b2efbb77e747df1
jaarmore <jaarmore@gmail.com>
2019-09-26 20:22:02 +0000

    Fix code on 7-print_diagonal.c file

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,13 +15,13 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j <= i; j++)
 			{
 				if (i == j)
 				{
 					_putchar(92);
 				}
-				else
+				if (j < i)
 				{
 					_putchar(' ');
 				}
