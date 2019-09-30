2d0a026d47dbfca7232b76cb96c9665de20c1094
jaarmore <jaarmore@gmail.com>
2019-09-30 20:04:01 +0000

    Adding 1-swap.c file to repository

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * swap_int - Function that swaps the values of two integers.
+ * @a: Address of pointer *a
+ * @b: Address of pointer *b
+*/
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -3,3 +3,6 @@
 
 ## 0-reset_to_98.c
 > Function that takes a pointer to an int as parameter and updates the value it points to 98.
+
+## 1-swap.c
+> Function that swaps the values of two integers.
