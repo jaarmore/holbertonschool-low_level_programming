392c1671eb279d1fb62adc01fdcadfa55e7e792c
jaarmore <jaarmore@gmail.com>
2019-10-03 20:56:18 +0000

    Fix code on 3-strcmp.c file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,7 +17,7 @@ int _strcmp(char *s1, char *s2)
 	}
 	else
 	{
-		while (result != 0 && s1[i] != '\0')
+		while (result != 0 && s1[i] != '\0' && s2[i] != '\0')
 		{
 			result = s1[i] - s2[i];
 			i++;
