abbc160a2be0edcae72a13c4c94a923a1d6ceeaa
jaarmore <jaarmore@gmail.com>
2019-10-13 03:41:19 +0000

    Adding file 0-whatsmyname.c to repository

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point.
+ * @argc: number of arguments.
+ * @argv: String of characters
+ * Return: always return 0.
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,5 @@
+# 0x0A. C - argc, argv
+> This topics is about how to use arguments passed to your programs.
+
+## 0-whatsmyname.c
+> Write a program that prints its name, followed by a new line.
