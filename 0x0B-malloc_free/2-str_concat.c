93cd199babd93879d0f8e9325a42b77d2e122aad
jaarmore <jaarmore@gmail.com>
2019-10-16 03:09:29 +0000

    Fix code on 2-str_concat.c file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,12 +19,12 @@ char *str_concat(char *s1, char *s2)
 	len1 = 0;
 	while (s1[len1] != '\0')
 		len1++;
-	len1++;
+
 	len2 = 0;
 	while (s2[len2] != '\0')
 		len2++;
-	len2++;
-	ptr = malloc(sizeof(char) * (len1 + len2));
+
+	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (ptr == NULL)
 	{
 		return (NULL);
