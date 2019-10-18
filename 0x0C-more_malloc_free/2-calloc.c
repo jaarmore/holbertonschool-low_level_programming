30c1ad5987928bb401b89e0ea7af50ed270dd5f4
jaarmore <jaarmore@gmail.com>
2019-10-18 02:44:18 +0000

    Fix code on 2-calloc.c file

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,8 +21,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 
-	for (i = 0; i < nmemb; i++)
-		ptr[i] = '0';
+	for (i = 0; i < (size * nmemb); i++)
+		ptr[i] = 48;
 
 	return (ptr);
 }
