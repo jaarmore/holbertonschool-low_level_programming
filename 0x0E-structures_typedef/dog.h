42e636b73f4bbe1dac7ce17a2d5228ffbefd5416
jaarmore <jaarmore@gmail.com>
2019-10-21 19:53:02 +0000

    Adding dog.h, and README.md files to repository

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - define a new type of element of type dog.
+ * @name: define a name of dog
+ * @age: define the age of dog
+ * @owner: define the owner of dog.
+ *
+ * Description: this struct create a new structure of type dog.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
