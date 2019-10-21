c6eb3e70582b83719121fdd457addd6d13d294fe
jaarmore <jaarmore@gmail.com>
2019-10-21 20:11:34 +0000

    Adding 1-init_dog.c file to repository

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - function that initializes the struct dog.
+ * @d: pointer to struct.
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: owner of dog.
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age =  age;
+	d->owner = owner;
+}
