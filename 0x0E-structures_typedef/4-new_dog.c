029ff3262e54cb745275a1a4dcea95dc74becf02
jaarmore <jaarmore@gmail.com>
2019-10-21 22:37:13 +0000

    Adding 4-new_dog.c file to repository

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,32 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * new_dog - function that creates a new dog.
+ * @name: name of the new dog.
+ * @age: age of the new dog.
+ * @owner: new owner of the dog.
+ * Return: pointer to point a new struct dog.
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ndog;
+
+	if (name == NULL)
+		return (NULL);
+
+	if (owner == NULL)
+		return (NULL);
+
+	ndog = malloc(sizeof(dog_t));
+
+	if (ndog == NULL)
+	{
+		return (NULL);
+	}
+	ndog->name = name;
+	ndog->age = age;
+	ndog->owner = owner;
+
+	return (ndog);
+}
