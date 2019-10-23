594d1012c675104e5a4222e8a564fec5627686af
jaarmore <jaarmore@gmail.com>
2019-10-23 20:38:20 +0000

    Adding 0-print_name.c, function_pointers.h files to repository

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - function that prints a name using function pointer.
+ * @name: pointer to char.
+ * @f: pointer function that call to print function.
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	void (*ptr_f)(char *);
+
+	ptr_f = f;
+	ptr_f(name);
+}
