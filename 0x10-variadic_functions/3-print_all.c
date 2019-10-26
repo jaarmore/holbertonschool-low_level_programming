2affd9abeb1d9e51f1d97681a8dc5fd971e03714
jaarmore <jaarmore@gmail.com>
2019-10-26 19:56:32 +0000

    Adding function is_valid in 3-print_all.c file

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,19 +2,40 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
+ * is_valid - function that checks the next argument passed to function
+ * @c: value to be checked.
+ * Return: 0 if no exist, 1 otherwise.
+*/
+int is_valid(char c)
+{
+	char value[] = "cifs";
+	int i, cont = 0;
+
+	if (c == '\0')
+		return (0);
+
+	for (i = 0; i < 4; i++)
+	{
+		if (c == value[i])
+		{
+			cont++;
+		}
+	}
+	return (cont);
+}
+/**
  * print_all - function that prints anything.
  * @format: is a list of types of arguments passed to function.
 */
 void print_all(const char * const format, ...)
 {
-	int i = 0, count;
+	int i = 0;
 	va_list all;
 	char *str;
 
 	va_start(all, format);
 	while ((format[i] != '\0') && (format != NULL))
 	{
-		count = 0;
 		switch (format[i])
 		{
 		case 'c':
@@ -35,11 +56,8 @@ void print_all(const char * const format, ...)
 			}
 			printf("(nil)");
 			break;
-		default:
-			++count;
-			break;
 		}
-		if ((count == 0) && format[(i + 1)] != '\0')
+		if (is_valid(format[(i + 1)]) > 0)
 			printf(", ");
 		i++;
 	}
