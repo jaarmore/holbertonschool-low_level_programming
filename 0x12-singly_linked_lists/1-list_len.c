9ad860502e6a6edfec8332309896a1c11f561966
jaarmore <jaarmore@gmail.com>
2019-10-31 18:56:48 +0000

    Adding 1-list_len.c file to repository

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * list_len - function that returns the number of elements in a linked list.
+ * @h: first element of the list.
+ * Return: numbers of elements of the list.
+*/
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+
+	if (h == NULL)
+		return (-1);
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
