ad60143459e8ed50c9935334c507b36da1b70971
jaarmore <jaarmore@gmail.com>
2019-10-31 23:37:29 +0000

    Adding 4-free_list.c file to repository

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_list - function that frees a linked list.
+ * @head: first element of the list.
+*/
+void free_list(list_t *head)
+{
+	list_t *node;
+
+	while (head != NULL)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
