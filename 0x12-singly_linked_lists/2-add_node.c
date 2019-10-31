bc794d758b1c3a704a47f58b0bca50865a908987
jaarmore <jaarmore@gmail.com>
2019-10-31 21:05:55 +0000

    Adding 2-add_node.c file to repository

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * add_node - Function that adds a new node at the beginning of aa linked list.
+ * @head: head of the linked list.
+ * @str: string to be copied as data on the node.
+ * Return: the address of the new node created.
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = NULL;
+
+	if ((*head) == NULL)
+	{
+		(*head) = (list_t *)malloc(sizeof(list_t));
+		(*head)->str = strdup(str);
+		(*head)->len = strlen(str);
+	}
+	else
+	{
+		new = (list_t *)malloc(sizeof(list_t));
+		new->str = strdup(str);
+		new->len = strlen(str);
+		new->next = (*head);
+		(*head) = new;
+	}
+	return ((*head));
+}
