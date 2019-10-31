3cf4e4534803f13a22386d9a86fe21cae8b9aac0
jaarmore <jaarmore@gmail.com>
2019-10-31 21:14:38 +0000

    Evaluate NULL if fail on 2-add_node.c file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,12 +12,16 @@ list_t *add_node(list_t **head, const char *str)
 	if ((*head) == NULL)
 	{
 		(*head) = (list_t *)malloc(sizeof(list_t));
+		if ((*head) == NULL)
+			return (NULL);
 		(*head)->str = strdup(str);
 		(*head)->len = strlen(str);
 	}
 	else
 	{
 		new = (list_t *)malloc(sizeof(list_t));
+		if (new == NULL)
+			return (NULL);
 		new->str = strdup(str);
 		new->len = strlen(str);
 		new->next = (*head);
