1574a2d92d3943c56ce5d5eda13cd69fa91a7a92
jaarmore <jaarmore@gmail.com>
2019-11-03 18:57:35 +0000

    Adding 100-first.c file to repository

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * beforeFunc - function that print text before main function
+ *
+ */
+void beforeFunc(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
