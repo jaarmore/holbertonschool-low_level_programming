9072e26589451e7e1b276599eadb54273a6ad7e1
jaarmore <jaarmore@gmail.com>
2019-11-05 11:49:02 +0000

    Evaluating head == NULL on 5-free_listint2.c file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,12 +7,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *node;
 
+	if ((*head) == NULL)
+		return;
+
 	while ((*head) != NULL)
 	{
 		node = (*head);
 		(*head) = (*head)->next;
 		free(node);
 	}
-	free((*head));
 	(*head) = NULL;
 }
