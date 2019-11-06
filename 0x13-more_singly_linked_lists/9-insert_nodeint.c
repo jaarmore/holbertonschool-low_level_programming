23c27b04f21ea312aacae0bb400aca32e67a9d99
jaarmore <jaarmore@gmail.com>
2019-11-06 03:36:02 +0000

    Evaluated temp != NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,7 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	for (cont = 0; cont < (idx - 1); cont++)
 		temp = temp->next;
-	if (cont == (idx - 1))
+	if (cont == (idx - 1) && temp != NULL)
 	{
 		node->next = temp->next;
 		temp->next = node;
