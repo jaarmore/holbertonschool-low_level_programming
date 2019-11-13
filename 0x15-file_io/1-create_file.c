064801a36ee1b8b38c701754fa86c00c0e3ebde0
jaarmore <jaarmore@gmail.com>
2019-11-13 04:46:06 +0000

    Adding 1-create_file.c file to repository

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * create_file - function that creates a txt file.
+ * @filename: the name of file to create.
+ * @text_content: text to add to file.
+ * Return: 1 if success or -1 if fails.
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd1, fd2, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd1 = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	if (fd1 == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+
+	fd2 = write(fd1, text_content, len);
+	close(fd1);
+	if (fd2 == -1)
+	{
+		return (-1);
+	}
+	return (1);
+}
