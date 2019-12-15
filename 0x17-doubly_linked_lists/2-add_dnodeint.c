62cba9a63ba5ba9f9a88aaa455512add2c803023
jaarmore <jaarmore@gmail.com>
2019-12-15 05:28:42 +0000

    Fix malloc code on 2-add_dnodeint.c file

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,10 +7,15 @@
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *node = new_node(n);
+	dlistint_t *node = NULL;
 
+	node = malloc(sizeof(dlistint_t *));
 	if (node == NULL)
 		return (NULL);
+
+	node->n = n;
+	node->prev = NULL;
+	node->next = NULL;
 
 	if ((*head) == NULL)
 	{
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -20,7 +20,6 @@ typedef struct dlistint_s
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
 } dlistint_t;
-dlistint_t *new_node(int data);
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
