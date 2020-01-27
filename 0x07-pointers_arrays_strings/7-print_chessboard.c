e570e203a074a1fcd3204aed90d281777cd9d211
jaarmore <jaarmore@gmail.com>
2020-01-27 06:49:11 +0000

    adding 7-print_chessboard.c file to repository

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_chessboard - function that prints the chessboard
+ * @a: chessboard to print.
+ * Return: Nothing
+*/
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	char *ptr;
+
+	ptr = (char *)a;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", *ptr++);
+		}
+		printf("\n");
+	}
+}
