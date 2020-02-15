4db410c6fd0435271bdf852dcabe7f94c39c8901
Jackson Moreno <43226501+jaarmore@users.noreply.github.com>
2020-02-15 14:16:52 -0500

    refactoring code on file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,57 +1,56 @@
-#define _POSIX_C_SOURCE 200809L
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#define READ_NBYTES 1204
+#include "holberton.h"
 /**
- * main - program to copy content of file to another file.
- * @argc: count of arguments to program
- * @argv: array of arguments to program
- * Return: EXIT_SUCCESS on success, exit with error number, otherwise.
-*/
-int main(int argc, char *argv[])
+ * main - C program that copied file content from one to another
+ * @argc: size of array argv and number of command line arguments
+ * @av: An  array  of size argc
+ * Return: Always successful
+ */
+int main(int argc, char *av[])
 {
-	char *file_from, *file_to;
-	char buf[READ_NBYTES];
-	ssize_t r;
-	int fdr, fdw, c = 0;
+	int file1, file2, fileread, filewrite;
+	char buffer[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	file_from = argv[1];
-	file_to = argv[2];
-	fdr = open(file_from, O_RDONLY);
-	if (fdr == -1)
+	file1 = open(av[1], O_RDONLY);
+	if (file1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-			file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	fdw = open(file_to, O_CREAT | O_TRUNC | O_WRONLY, 00664);
-	if (fdw == -1)
+	file2  = open(av[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
+	if (file2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	while ((r = read(fdr, buf, READ_NBYTES)))
-		write(fdw, buf, r);
-	if (close(fdw))
+	do  {
+		fileread = read(file1, buffer, 1024);
+		if (fileread ==  -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+		filewrite = write(file2, buffer, fileread);
+		if (filewrite == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	} while (fileread == 1024);
+
+	if (close(file1) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdw);
-		c = 1;
-	}
-	if (close(fdr))
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdr);
-		c = 1;
-	}
-	if (c)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1);
 		exit(100);
-	exit(EXIT_SUCCESS);
+	}
+	if (close(file2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2);
+		exit(100);
+	}
+	return (0);
 }
