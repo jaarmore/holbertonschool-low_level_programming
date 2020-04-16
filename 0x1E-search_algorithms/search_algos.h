710b7102e6ab463e1512bed88ca29615c0263f68
jaarmore <jaarmore@gmail.com>
2020-04-16 18:13:34 +0000

    fix librarys on file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,8 @@
 #ifndef _SEARCH_ALGOS_H_
 #define _SEARCH_ALGOS_H_
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 #endif
